781e3a3c194011478877ef44f0c2e3dd93d6c949
pascalwitkowski <pascalwi@pm.me>
2022-04-06 12:44:48 +0200

    Backend setup

---
+++ backend/src/main/kotlin/lise/uebungsprojekt/UebungsprojektApplication.kt
@@ -0,0 +1,11 @@
+package lise.uebungsprojekt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UebungsprojektApplication
+
+fun main(args: Array<String>) {
+	runApplication<UebungsprojektApplication>(*args)
+}
