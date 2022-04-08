bb5567b1a4edca4bbafff9346f71553cbab306e9
pascalwitkowski <pascalwi@pm.me>
2022-04-08 19:49:16 +0200

    Frontend setup with MongoDB connection to get game data

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GameDetail from './pages/GameDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <GameDetail/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/GameDetail.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default class GameDetail extends React.Component {
+    state = {
+      name: "",
+      releaseDate: "",
+      dev: ""
+    }
+
+    componentDidMount() {
+      fetch("http://localhost:8080/game/624eb25731bdc44005f53105")
+        .then(res => res.json())
+        .then(
+          result => {
+            this.setState(result);
+          },
+          e => {
+            this.setState({});
+          }
+        )
+    }
+    
+    render() {
+      return (
+        <div className="game-detail">
+          <h1>{this.state.name}</h1>
+          <p>{this.state.releaseDate}</p>
+          <p>{this.state.dev}</p>
+        </div>
+      );
+    }
+  }
