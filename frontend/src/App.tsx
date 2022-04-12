b06e6919e3cfade05bad59a1fe58928126a9c83a
pascalwitkowski <pascalwi@pm.me>
2022-04-12 14:05:42 +0200

    Added sass

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.sass';
 import GameDetail from './pages/GameDetail';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <GameDetail/>
     </div>
   );
