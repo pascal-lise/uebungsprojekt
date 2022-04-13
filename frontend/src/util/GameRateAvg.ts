bd04e700e8d81928c8da593f9b6e32a701e44389
pascalwitkowski <pascalwi@pm.me>
2022-04-13 13:38:00 +0200

    Helper functions for data display

---
+++ frontend/src/util/GameRateAvg.ts
@@ -0,0 +1,7 @@
+import Game from '../model/Game';
+
+export function getGameRateAvg(game: Game): number {
+    const sum: number = game.ratings.reduce((sum: number, current: any) => sum + current.value, 0)       
+    const ratings: number =  game.ratings.length
+    return ratings !== 0 ? sum / game.ratings.length : 0    
+}
---
+++ frontend/src/util/SortGames.ts
@@ -0,0 +1,14 @@
+import { getGameRateAvg } from './GameRateAvg'
+import Game from '../model/Game';
+
+export default function compareGames(a: Game, b: Game): number {
+    const avgA: number = getGameRateAvg(a)
+    const avgB: number = getGameRateAvg(b)
+    const timeA: number = new Date(a.releaseDate).getTime()
+    const timeB: number = new Date(b.releaseDate).getTime()
+    if(avgA < avgB) return 1
+    if(avgA > avgB) return -1
+    if(timeA < timeB) return 1
+    if(timeA > timeB) return -1
+    return 1
+}
