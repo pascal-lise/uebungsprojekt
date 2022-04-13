82b23ed7608a092abe5d769584ddd7148b06f03c
pascalwitkowski <pascalwi@pm.me>
2022-04-13 13:02:54 +0200

    Added picture name attribute for picture display

--- backend/src/main/kotlin/lise/uebungsprojekt/model/Game.kt
+++ backend/src/main/kotlin/lise/uebungsprojekt/model/Game.kt
@@ -6,4 +6,4 @@ import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
 @Document
-data class Game(@Id val id: ObjectId, val name: String, val releaseDate: Date, val dev: String, var ratings: List<Rating>?)
+data class Game(@Id val id: ObjectId, val name: String, val releaseDate: Date, val dev: String, var ratings: List<Rating>?, val picName: String?)
