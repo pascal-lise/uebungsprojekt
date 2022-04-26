8c0c18981ba5ada12a592a18b1df9285a6d5d807
Pascal Witkowski <pascal.witkowski@lise.de>
2022-04-26 11:51:42 +0200

    Search games input field

--- frontend/src/pages/GameList.tsx
+++ frontend/src/pages/GameList.tsx
@@ -1,4 +1,4 @@
-import { Grid, SelectChangeEvent } from '@mui/material';
+import { Grid, SelectChangeEvent, TextField } from '@mui/material';
 import { useEffect, useState } from 'react';
 import GameCard from '../components/GameCard';
 import Game from '../model/Game';
@@ -13,25 +13,34 @@ export default function GameList() {
   const [consoles, setConsoles] = useState<Console[]>([])
   const [selectedConsoles, setSelectedConsoles] = useState<string[]>([])
   const [games, setGames] = useState<Game[]>([])
+  const [searchedGames, setSearchedGames] = useState<Game[]>([])
 
   function handleConsoleFilterChange (event: SelectChangeEvent<typeof selectedConsoles>) {
     setSelectedConsoles(event.target.value as string[])
   }
 
+  function handleGameSearchChange (event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchedGames(games.filter((game: Game) => game.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
+
   useEffect(() => { 
     ConsoleController.getConsoles().then(setConsoles)    
-    GameController.getGames().then(setGames)
+    GameController.getGames().then(games => {
+      setGames(games)
+      setSearchedGames(games)
+    })
   }, [])
 
   return (
     <div className='game-list'>
       <div className='game-list-settings'>
         <ConsoleFilter consoles={consoles} selectedConsoles={selectedConsoles} handleChange={handleConsoleFilterChange}/>
+        <TextField id="outlined-basic" label="Search Game" variant="outlined" margin='dense' onChange={handleGameSearchChange}/>
       </div>
       <div className='game-list-grid'>
         <Grid container spacing={5}>
           {
-            games
+            searchedGames
             .sort((a: Game, b: Game) => compareGames(a, b))
             .filter((game: Game) => game.consoles.some((c: Console) => selectedConsoles.includes(c.name) || selectedConsoles.length === 0))
             .map((game: Game, idx: number) => {
