330f7cb2f128a7a1b0c485bca29051633715fe8a
Pascal Witkowski <pascal.witkowski@lise.de>
2022-04-26 11:16:59 +0200

    Changed title

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,13 +1,12 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import React from 'react';
-import './Header.sass'
 
 export default class Header extends React.Component {
   render() {
     return (
         <AppBar position='static' className='header'>
           <Toolbar>
-            <Typography variant='h5'>Spiele-Plattform</Typography>
+            <Typography variant='h5'>Games Platform</Typography>
           </Toolbar>
         </AppBar>
     )
