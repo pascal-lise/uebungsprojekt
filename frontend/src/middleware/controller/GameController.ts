5be5ded61d085402e04bfa663d1d1911b7a2fcf0
Pascal Witkowski <pascal.witkowski@lise.de>
2022-04-26 14:31:50 +0200

    Moved sort and filter logic to controller

--- frontend/src/middleware/controller/GameController.ts
+++ frontend/src/middleware/controller/GameController.ts
@@ -1,9 +1,14 @@
 import Game from "../../model/Game"
+import compareGames from "../../util/SortGames"
 import GameService from '../service/GameService'
+import Console from '../../model/Console';
 
 export default {
     getGameById: async (id: string): Promise<Game> => prepareGame(await GameService.getGameById(id)),
     getGames: async (): Promise<Game[]> => (await GameService.getGames()).map((game: Game) => prepareGame(game)),
+    sortGames: (games: Game[]): Game[] => games.sort((a: Game, b: Game) => compareGames(a, b)),
+    filterByConsoles: (games: Game[], consoles: string[]): Game[] => games.filter((game: Game) => 
+        game.consoles.some((c: Console) => consoles.includes(c.name) || consoles.length === 0))
 }
 
 function prepareGame(game: Game) {
--- frontend/src/pages/GameList.tsx
+++ frontend/src/pages/GameList.tsx
@@ -8,6 +8,7 @@ import compareGames from '../util/SortGames';
 import ConsoleFilter from '../components/ConsoleFilter';
 import GameController from '../middleware/controller/GameController';
 import ConsoleController from '../middleware/controller/ConsoleController';
+import GameSearchBar from '../components/GameSearchBar';
 
 export default function GameList() {
   const [consoles, setConsoles] = useState<Console[]>([])
@@ -25,7 +26,7 @@ export default function GameList() {
 
   useEffect(() => { 
     ConsoleController.getConsoles().then(setConsoles)    
-    GameController.getGames().then(games => {
+    GameController.getGames().then((games: Game[]) => {
       setGames(games)
       setSearchedGames(games)
     })
@@ -35,14 +36,13 @@ export default function GameList() {
     <div className='game-list'>
       <div className='game-list-settings'>
         <ConsoleFilter consoles={consoles} selectedConsoles={selectedConsoles} handleChange={handleConsoleFilterChange}/>
-        <TextField id="outlined-basic" label="Search Game" variant="outlined" margin='dense' onChange={handleGameSearchChange}/>
+        <GameSearchBar handleChange={handleGameSearchChange} />
       </div>
       <div className='game-list-grid'>
         <Grid container spacing={5}>
           {
-            searchedGames
-            .sort((a: Game, b: Game) => compareGames(a, b))
-            .filter((game: Game) => game.consoles.some((c: Console) => selectedConsoles.includes(c.name) || selectedConsoles.length === 0))
+            GameController.sortGames(
+              GameController.filterByConsoles(searchedGames, selectedConsoles))
             .map((game: Game, idx: number) => {
               return (                
                 <Grid item key={idx} xs={4}>
