721783c991cce295d565d27da003ae62d06322a6
Pascal Witkowski <pascal.witkowski@lise.de>
2022-05-27 15:21:28 +0200

    UPPW-48: keycloak backend initial setup

---
+++ backend/src/main/kotlin/lise/uebungsprojekt/config/KeycloakSecurityConfig.kt
@@ -0,0 +1,37 @@
+package lise.uebungsprojekt.config
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+
+@KeycloakConfiguration
+class KeycloakSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        super.configure(http)
+        http.authorizeRequests()
+            .anyRequest()
+            .permitAll()
+    }
+
+    @Autowired
+    @Throws(Exception::class)
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        val keycloakAuthenticationProvider: KeycloakAuthenticationProvider = keycloakAuthenticationProvider()
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        auth.authenticationProvider(keycloakAuthenticationProvider)
+    }
+
+    @Bean
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+    }
+}
