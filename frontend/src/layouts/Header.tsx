0acd8e79919f5f1b4d6b0c2a6befc4098beb1f43
Pascal Witkowski <pascal.witkowski@lise.de>
2022-05-31 11:10:20 +0200

    UPPW-48: avoided duplication in login logout button creation

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -5,16 +5,19 @@ import { useAuth } from "react-oidc-context";
 export default function Header () {
   const auth = useAuth();
 
+  function buttonLoginLogout() {
+    const authenticated = auth.isAuthenticated
+    return <Button style={{ right: '2rem', position: 'fixed' }} onClick={() => authenticated ? void auth.signoutRedirect() : void auth.signinRedirect()}>
+      { authenticated ? 'Log out' : 'Log in' }
+    </Button>
+  }
+
   return (
     <header className='header'>
       <AppBar position='static' className='header'>
         <Toolbar>
           <Typography variant='h6'>Games Platform</Typography>
-          { 
-            auth.isAuthenticated ?
-              <Button style={{ right: '2rem', position: 'fixed' }} onClick={() => void auth.signoutRedirect()}>Log out</Button> :
-              <Button style={{ right: '2rem', position: 'fixed' }} onClick={() => void auth.signinRedirect()}>Log in</Button> 
-          }
+            { buttonLoginLogout() }
         </Toolbar>
       </AppBar>
     </header>
