1a0890c7860d4c095e4c82114fc435cb92249908
Pascal Witkowski <pascal.witkowski@lise.de>
2022-05-31 11:09:44 +0200

    UPPW-48: moved oicd config to .env

--- frontend/src/config/oidcConfig.ts
+++ frontend/src/config/oidcConfig.ts
@@ -1,6 +1,11 @@
+const authority = process.env.REACT_APP_KEYCLOAK_REALM
+const clientId = process.env.REACT_APP_KEYCLOAK_CLIENT_ID
+const redirectUri = process.env.REACT_APP_KEYCLOAK_REDIRECT_URI
+const postLogoutRedirectUri = process.env.REACT_APP_KEYCLOAK_POST_LOGOUT_REDIRECT_URI
+
 export default {
-    authority: "http://localhost:2000/auth/realms/master",
-    client_id: "public",
-    redirect_uri: "http://localhost:3000",
-    post_logout_redirect_uri: "http://localhost:3000"
+    authority: authority ? authority : '',
+    client_id: clientId ? clientId : '',
+    redirect_uri: redirectUri ? redirectUri : '',
+    post_logout_redirect_uri: postLogoutRedirectUri ? postLogoutRedirectUri : '',
 }
