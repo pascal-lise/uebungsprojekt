956763224409685892c8a2c3a062c9a709165ba7
Pascal Witkowski <pascal.witkowski@lise.de>
2022-05-31 14:06:58 +0200

    UPPW-53: user authorization for rating API

--- backend/src/main/kotlin/lise/uebungsprojekt/config/KeycloakSecurityConfig.kt
+++ backend/src/main/kotlin/lise/uebungsprojekt/config/KeycloakSecurityConfig.kt
@@ -6,13 +6,19 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 
 @KeycloakConfiguration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true,
+    securedEnabled = true,
+    jsr250Enabled = true)
 class KeycloakSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
@@ -22,6 +28,7 @@ class KeycloakSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
             .csrf()
             .disable()
             .authorizeRequests()
+            .antMatchers("/ratings/add/").hasRole("user")
             .anyRequest()
             .permitAll()
     }
--- backend/src/main/kotlin/lise/uebungsprojekt/controller/RatingController.kt
+++ backend/src/main/kotlin/lise/uebungsprojekt/controller/RatingController.kt
@@ -4,6 +4,7 @@ import lise.uebungsprojekt.model.Rating
 import lise.uebungsprojekt.service.RatingService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,6 +15,7 @@ import javax.validation.Valid
 @CrossOrigin
 class RatingController(private val ratingService: RatingService) {
     @PostMapping("/ratings/add")
+    @PreAuthorize("hasRole('user')")
     fun addRating(@Valid @RequestBody rating: Rating): ResponseEntity<String> {
         ratingService.addRating(rating)
         return ResponseEntity.status(HttpStatus.CREATED).build()
